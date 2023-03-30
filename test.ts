9fa702a3c92a70ba61b27908692d91383183c0f4
Max Schmitt <max@schmitt.mx>
2023-03-30 13:34:52 +0200

    Initial commit

---
+++ test.ts
@@ -0,0 +1,31 @@
+import assert from 'node:assert';
+
+{
+  class Foo {
+    constructor(private readonly incoming: number) { }
+    value = this.incoming;
+  }
+
+  console.log(new Foo(42).value)
+  assert.equal(new Foo(42).value, 42);
+}
+
+{
+  class Base {
+    p1: string;
+    p2: string;
+    constructor(p1: string, p2: string) {
+      this.p1 = p1;
+      this.p2 = p2;
+    }
+  }
+
+  class Derived extends Base {
+    p1: string;
+    declare p2: string;
+  }
+
+  const d = new Derived('value1', 'value2');
+  assert.equal(d.p1, undefined);
+  assert.equal(d.p2, 'value2');
+}
