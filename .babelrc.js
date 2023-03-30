030f9d3826242437644a2da37812bf0b4ad19885
Max Schmitt <max@schmitt.mx>
2023-03-30 13:57:27 +0200

    update

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    ['@babel/plugin-transform-typescript', { allowDeclareFields: true }],
+    ['@babel/plugin-transform-typescript', { onlyRemoveTypeImports: true, allowDeclareFields: true }],
     '@babel/plugin-proposal-class-properties',
   ],
 };
--- test.ts
+++ test.ts
@@ -10,21 +10,16 @@ const assert = require('assert');
 }
 
 {
-  class Base {
-    p1: string;
-    p2: string;
+  class Derived {
     constructor(p1: string, p2: string) {
       this.p1 = p1;
       this.p2 = p2;
     }
-  }
-
-  class Derived extends Base {
-    p1: string;
+    declare p1: string;
     declare p2: string;
   }
 
   const d = new Derived('value1', 'value2');
-  assert.equal(d.p1, undefined);
+  assert.equal(d.p1, 'value1');
   assert.equal(d.p2, 'value2');
 }
